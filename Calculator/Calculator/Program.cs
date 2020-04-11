82e50cd7326f3d06743e47adf530344de103585e
Anton St <antony.stasilevich@gmail.com>
2020-04-11 20:10:00 +0300

    Add calculator's program code

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose the mathematic operation:");
+            Console.WriteLine("1) Addition numbers");
+            Console.WriteLine("2) Substraction numbers");
+            Console.WriteLine("3) Division numbers");
+            Console.WriteLine("4) Multiplication numbers");
+            Console.WriteLine("5) Exit");
+            Console.Write("Your choice: ");
+            string result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                AdditionNumbers();
+                return true;
+            }
+
+            if (result == "2")
+            {
+                SubtractionNumbers();
+                return true;
+             }
+            
+            if (result == "3")
+            {
+                Division();
+                return true;
+            }
+            if (result == "4")
+            {
+                Multiplication();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
