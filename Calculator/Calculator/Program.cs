8d33dd71477d4736e4eab6f9f8efb69d16afec18
anton88st <antony.stasilevich@gmail.com>
2020-04-23 15:22:42 +0300

    Add Base class and inheritors

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,91 +10,80 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            bool displayMemu = true;
-            while (displayMemu)
-            {
-                Console.Clear();
-                NameApplication("");
-                Console.WriteLine("Type the first number:");
-                string firstNumber = Console.ReadLine();
-                if (firstNumber == "q")
-                {
-                    displayMemu = false;
-                }
-                else
-                {
-                    Console.WriteLine("Type the second  number");
-                    string secondNumber = Console.ReadLine();
-                    if (secondNumber == "q")
-                    {
-                        displayMemu = false;
-                    }
-                    else
-                    {
-                        Calculation myCalc = new Calculation { num1 = double.Parse(firstNumber), num2 = double.Parse(secondNumber) };
-                        myCalc.Result();
-                        displayMemu = true;
-                    }
-                }
+            Console.WriteLine("Select operation: ");
 
-            }
-        }
-        private static void NameApplication(string nameOperation)
-        {
-            string nameApplication = "Welcome: Console Calculator.\n";
-            Console.Clear();
-            Console.SetCursorPosition((Console.WindowWidth - nameApplication.Length) / 2, Console.CursorTop);
-            Console.WriteLine(nameApplication);
-        }
-    }
-    class Calculation
-    {
-        public double num1;
-        public double num2;
-        public double Addition()
-        {
-            return num1 + num2;
-        }
-        public double Division()
-        {
-            return num1 / num2;
-        }
-        public double Subtraction()
-        {
-            return num1 - num2;
-        }
-        public double Multiplication()
-        {
-            return num1 * num2;
-        }
-        public void Result()
-        {
-            Console.WriteLine("Select a mathematic operation:");
-            Console.WriteLine("1) Addition numbers");
-            Console.WriteLine("2) Substraction numbers");
-            Console.WriteLine("3) Division numbers");
-            Console.WriteLine("4) Multiplication numbers");
-            string choice = Console.ReadLine();
-            switch (choice)
+            string select = Console.ReadLine();
+            switch (select)
             {
-                case "1":
-                    Console.WriteLine("Result:{0}+{1}={2}", num1, num2, Addition());
+                case "a":
+                    Addition addition = new Addition { Number1 = 1, Number2 = 2 };
+                    addition.Calculation();
                     break;
-                case "2":
-                    Console.WriteLine("Result:{0}-{1}={2}", num1, num2, Subtraction());
+                case "s":
+                    Subtraction subtraction = new Subtraction { Number1 = 1, Number2 = 2 };
+                    subtraction.Calculation();
                     break;
-                case "3":
-                    Console.WriteLine("Result:{0}/{1}={2}", num1, num2, Division());
+                case "m":
+                    Multiplication multiplication = new Multiplication { Number1 = 1, Number2 = 2 };
+                    multiplication.Calculation();
                     break;
-                case "4":
-                    Console.WriteLine("Result:{0}*{1}={2}", num1, num2, Multiplication());
-                    break;
-                default:
-                    Console.WriteLine("Error: Input data is not valid. Press Enter and select option from list above");
-                    Console.ReadLine();
+                case "d":
+                    Division division = new Division { Number1 = 1, Number2 = 2 };
+                    division.Calculation();
                     break;
             }
             Console.ReadLine();
+            return;
+        }
+    }
+    abstract class Base
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double Result { get; set; }
+        public abstract void Calculation();
+    }
+    class Addition : Base
+    {
+        public override void Calculation()
+        {
+            Result = Number1 + Number2;
+            Console.WriteLine("Result:{0}", Result);
+            return;
+        }
+    }
+    class Subtraction : Base
+    {
+        public override void Calculation()
+        {
+            Result = Number1 - Number2;
+            Console.WriteLine("Result:{0} ", Result);
+            return;
+        }
+    }
+    class Multiplication : Base
+    {
+        public override void Calculation()
+        {
+            Result = Number1 * Number2;
+            Console.WriteLine("Result:{0} ", Result);
+            return;
+        }
+    }
+    class Division : Base
+    {
+        public override void Calculation()
+        {
+            if (Number2 == 0)
+            {
+                throw new Exception("Number2 can't be zero");
+            }
+            else
+            {
+                Result = Number1 / Number2;
+                Console.WriteLine("Result:{0} ", Result);
+            }
+            return;
         }
     }
 }
